0a6ddac342336ea3a741a9cb066da4425835713f
Tyion-Parham <112714623+Tyion-Parham@users.noreply.github.com>
2024-08-29 09:50:31 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {   // line can be made using psvm -> tab
-        System.out.println("Hello World!");   // System.out.println() can be made using sout -> tab
-    }
-}
